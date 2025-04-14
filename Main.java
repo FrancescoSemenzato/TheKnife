0ae16522bb5f84969684b6defbdd3e6aeb330e33
semebellissimo <semebellissimo05@gmail.com>
2025-04-14 17:06:14 +0200

    Codice per stampare il file csv

--- Main.java
+++ Main.java
@@ -1,8 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Marrcco");
-        System.out.println("Procopio");
+        String path = "FileProva.csv";
+        String riga;
+        int j=0;
 
-        System.out.println("branch1");
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            while ((riga = br.readLine()) != null && j<10) {
+                String[] valori = riga.split(",");
+                for(String valore : valori)
+                    System.out.println(valore);
+                System.out.println();
+                j++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
