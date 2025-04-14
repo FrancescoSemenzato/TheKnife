da75a5ab3370b35641e9ce0f04d6c499a16f8284
semebellissimo <semebellissimo05@gmail.com>
2025-04-14 15:48:07 +0200

    Modifica

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Luca");
+        System.out.println("Marrcco");
         System.out.println("Procopio");
     }
 }
