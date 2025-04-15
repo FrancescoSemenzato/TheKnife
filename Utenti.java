02f286a3a217801d47654a03058f8591a84671da
Marco Barlera <143283524+ZonatedCord@users.noreply.github.com>
2025-04-15 16:28:26 +0200

    Aggiunto Utenti.java

---
+++ Utenti.java
@@ -0,0 +1,46 @@
+import java.time.LocalDate;
+
+public class Utenti {
+    private String Nome, Cognome,Username, Password, Domicilio, Ruolo;
+    private LocalDate DataDiNascita;
+    private boolean registrato; 
+
+    public Utenti(String Nome, String Cognome, String Username, String Password, String Domicilio, String Ruolo, LocalDate DataDiNascita) {
+        this.Nome = Nome;
+        this.Cognome = Cognome;
+        this.Username = Username;
+        this.Password = Password;
+        this.Domicilio = Domicilio;
+        this.Ruolo = Ruolo;
+        this.DataDiNascita = DataDiNascita;
+        this.registrato = true;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public String getCognome() {
+        return Cognome;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public String getDomicilio() {
+        return Domicilio;
+    }
+
+    public String getRuolo() {
+        return Ruolo;
+    }
+
+    public LocalDate getDataDiNascita() {
+        return DataDiNascita;
+    }
+}
