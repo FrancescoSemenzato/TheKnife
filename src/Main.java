40cdc326dee92aaccfcfef666002ca7ae6583bc5
Marco Barlera <143283524+ZonatedCord@users.noreply.github.com>
2025-04-16 15:41:06 +0200

    Lettura json

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,50 @@
 // java -cp "lib/json-simple-1.1.1.jar;bin" src.Main   
 
 package src;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDate;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "UTD8.csv";
+        String path = "files/csvjson.json";
         String riga;
         int j=0;
-        Utente utente1 = new Utente(
-            "Gino", 
-            "Apao", 
-            "masafsfario.f", 
-            "password123", 
-            "Via Roma 1, Milano", 
-            "Ristoratore", 
-            LocalDate.of(1985, 5, 15)
-        );
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            while ((riga = br.readLine()) != null && j<10) {
-                String[] valori = riga.split(",");
-                for(String valore : valori)
-                    System.out.println(valore);
-                System.out.println();
-                j++;
+            JSONParser parser = new JSONParser();
+            // Legge l'intero file come JSON (può essere un JSONArray o JSONObject)
+            Object jsonData = parser.parse(br);
+            
+            if (jsonData instanceof JSONArray) {
+                JSONArray jsonArray = (JSONArray) jsonData;
+                for (Object obj : jsonArray) {
+                    if (j >= 13) break; // Limita a 13 elementi
+                    if (obj instanceof JSONObject) {
+                        JSONObject jsonObj = (JSONObject) obj;
+                        System.out.println("Riga " + (j + 1) + ":");
+                        for (Object key : jsonObj.keySet()) {
+                            System.out.println("  " + key + ": " + jsonObj.get(key));
+                        }
+                        System.out.println();
+                        j++;
+                    }
+                }
+            } else if (jsonData instanceof JSONObject) {
+                // Se il file contiene un singolo JSONObject
+                JSONObject jsonObj = (JSONObject) jsonData;
+                System.out.println("Contenuto del JSON:");
+                for (Object key : jsonObj.keySet()) {
+                    System.out.println(key + ": " + jsonObj.get(key));
+                }
+            } else {
+                System.out.println("Formato JSON non supportato.");
             }
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
     }
